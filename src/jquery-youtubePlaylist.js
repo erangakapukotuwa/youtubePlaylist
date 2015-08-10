429d648c27032f6960006d1138316e2885486c56
Eranga Kapukotuwa <eranga@thinkcube.com>
2015-08-10 09:03:04 +0530

    Removed unwanted files to the repo

--- src/jquery-youtubePlaylist.js
+++ src/jquery-youtubePlaylist.js
@@ -2,12 +2,11 @@
     $.fn.youtubePlaylist = function( options ) {
         //default options
         var settings = $.extend({
-            // These are the defaults.
             chanelName: "",
             userName: "",
         }, options );
 
-        var channelName = 'MrErangalakmal'; //example
+        var channelName = 'MrErangalakmal';
         var channelTitle = '';
         var apiKey = 'AIzaSyBVr-2n-aTSnlhM4CU5eKtmSzr8hflJqwI';
 
@@ -27,24 +26,7 @@
             dataType: 'json'
             }).done(function(b) {
                 console.log(b);
-                // var b = a.items[0].contentDetails.relatedPlaylists.uploads,
-                //     chid = a.items[0].id,
-                //     nekpag = '';
-                // youtube_video_list(b, apikey, nekpag, channels_title, chid, channels_name)
             });
-
-            // var b = a.items[0].contentDetails.relatedPlaylists.uploads,
-            //     chid = a.items[0].id,
-            //     nekpag = '';
-            // youtube_video_list(b, apikey, nekpag, channels_title, chid, channels_name)
         });
-
-        
- 
-        // Greenify the collection based on the settings variable.
-        // return this.css({
-        //     color: settings.color,
-        //     backgroundColor: settings.backgroundColor
-        // });
     };
 }( jQuery ));
